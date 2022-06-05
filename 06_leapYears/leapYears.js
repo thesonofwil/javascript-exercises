7156380f41c360ed8704f2236f07a57a6431a1dc
thesonofwil <thesonofwil@gmail.com>
2022-06-05 15:55:28 -0500

    passes

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+	if (year % 4 !== 0) {
+		return false;
+	}
 
+	// Years divisible by 100 are not leap years, unless they are divisible by 400 as well
+	if (year % 100 === 0 && year % 400 !== 0) {
+		return false;
+	}
+
+	return true;
 };
 
 // Do not edit below this line
