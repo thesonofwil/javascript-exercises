1def64abf53fc8c7a87d8c5c8f710cc6eb0f5863
thesonofwil <thesonofwil@gmail.com>
2022-06-05 15:43:07 -0500

    passes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+	let arr = args[0]; // first item is array to iterate through
+	let resultArr = [];
 
+	arr.forEach((item) => {
+
+		// Check if arr item matches with any args, and push if not
+		if (!args.includes(item)) {
+			resultArr.push(item);
+		}
+	});
+
+	return resultArr;
 };
 
 // Do not edit below this line
